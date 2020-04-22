b471a82105bca9ce0ab3a5c8c1b79863e624a75e
zribera18 <zribera18@gmail.com>
2020-04-21 23:53:23 -0700

    first commit, working rotation controller

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+
+  public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 100f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      print(Input.GetAxis("Horizontal"));
+      print(Input.GetAxis("Vertical"));
+      transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+
+      //
+
+    }
+}
